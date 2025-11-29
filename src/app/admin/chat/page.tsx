b25683c106d497ebd83d8debdaad33214e634d8d
jonamix-ar <jonatanvilella@gmail.com>
2025-11-29 19:35:57 -0300

    Fixes title and clean

--- src/app/admin/chat/page.tsx
+++ src/app/admin/chat/page.tsx
@@ -1,13 +1,6 @@
 import { Metadata } from "next";
 import ChatContent from "@/components/chats/ChatContent";
 
-export const metadata: Metadata = {
-    title: "Next.js Messages | TailAdmin - Next.js Dashboard Template",
-    description:
-        "This is Next.js Messages page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
-    // other metadata
-};
-
 export default function Chat() {
     return <ChatContent />;
 }
